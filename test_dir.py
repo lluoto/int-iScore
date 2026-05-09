1ae0952025449614031572b1c16baf9ce9a9981d
Cuixi <a13707621@gmail.com>
2026-05-09 22:24:58 +0800

    chore: remove test/temp artifacts and intermediate frustratometer outputs
    - Remove int_iscore_temp/, src/int_iscore_temp/, output_test/
    - Remove frustratometer2-master *.done/ dirs (389MB test outputs)
    - Remove root test scripts, CSVs, logs, PNGs
    - Remove __pycache__ and .bak files
    - Update .gitignore with comprehensive patterns

--- test_dir.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import os
-print("CWD:", os.getcwd())
-print("output_test exists:", os.path.exists("./output_test"))
-print("output_test isdir:", os.path.isdir("./output_test"))
